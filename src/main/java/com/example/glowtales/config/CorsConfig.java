83dfc27bd9b1210d312cf6306c9844d031561beb
hayeongKo <polarpheno@icloud.com>
2024-08-18 03:24:15 +0900

    [FEAT] corsConfig 구현

---
+++ src/main/java/com/example/glowtales/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.glowtales.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 설정하는 것
+        config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
+        config.addAllowedHeader("*"); // 모든 header에 응답을 허용
+        config.addAllowedMethod("*"); // 모든 http method (GET, POST, DELETE, PATCH) 요청 허용
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
+}
