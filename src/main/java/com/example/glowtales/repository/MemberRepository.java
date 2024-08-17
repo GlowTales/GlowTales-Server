06c11b54b154e136c0dc56a91f00465e366c9bd4
hayeongKo <polarpheno@icloud.com>
2024-08-18 03:22:21 +0900

    [FEAT] findBy--- 추가

--- src/main/java/com/example/glowtales/repository/MemberRepository.java
+++ src/main/java/com/example/glowtales/repository/MemberRepository.java
@@ -4,5 +4,7 @@ import com.example.glowtales.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByName(String name);
+    Member findByEmail(String email);
 }
 
