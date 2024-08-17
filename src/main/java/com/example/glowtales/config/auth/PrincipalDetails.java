c1c7e802c2988364a274254aba3c4808ee2faf5b
hayeongKo <polarpheno@icloud.com>
2024-08-18 03:25:22 +0900

    [FEAT] principalDetails 구현 및 관련 service 구현

---
+++ src/main/java/com/example/glowtales/config/auth/PrincipalDetails.java
@@ -0,0 +1,81 @@
+package com.example.glowtales.config.auth;
+
+import com.example.glowtales.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private Member member;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+
+    public PrincipalDetails(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        member.getRoleList().forEach(r -> {
+            authorities.add(() -> r);
+        });
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
