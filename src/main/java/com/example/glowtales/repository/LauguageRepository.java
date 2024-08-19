e291fbfce078a9d0bcfb454b8467997951da7b0f
hayeongKo <polarpheno@icloud.com>
2024-08-20 02:24:08 +0900

    [FEAT] LanguageRepository, LearningLanguageRepository 생성

---
+++ src/main/java/com/example/glowtales/repository/LauguageRepository.java
@@ -0,0 +1,7 @@
+package com.example.glowtales.repository;
+
+import com.example.glowtales.domain.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LauguageRepository extends JpaRepository<Language, Long> {
+}
