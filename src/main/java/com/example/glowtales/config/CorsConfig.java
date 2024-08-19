8131bc646af86d1296d61946e3789076d8a112f0
hayeongKo <polarpheno@icloud.com>
2024-08-19 15:40:43 +0900

    [REFACTOR] allowedOrigins -> allowedOriginPatterns 수정

--- src/main/java/com/example/glowtales/config/CorsConfig.java
+++ src/main/java/com/example/glowtales/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 설정하는 것
-        config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
+        config.addAllowedOriginPattern("*"); // 모든 ip에 응답을 허용
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용
         config.addAllowedMethod("*"); // 모든 http method (GET, POST, DELETE, PATCH) 요청 허용
         source.registerCorsConfiguration("/api/**", config);
