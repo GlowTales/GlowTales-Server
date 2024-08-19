cf3312eb1f12c072a6b6e53ec3d4376c6a19ef4b
hayeongKo <polarpheno@icloud.com>
2024-08-20 02:27:41 +0900

    [FEAT] 회원 정보 추가 기입 API 구현

--- src/main/java/com/example/glowtales/controller/MemberController.java
+++ src/main/java/com/example/glowtales/controller/MemberController.java
@@ -1,15 +1,17 @@
 package com.example.glowtales.controller;
 
-import com.example.glowtales.dto.response.auth.LoginResponse;
+import com.example.glowtales.domain.ResultCode;
+import com.example.glowtales.dto.Result;
+import com.example.glowtales.dto.request.MemberForm;
+import com.example.glowtales.service.MemberService;
 import com.example.glowtales.service.oauth.KakaoService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.NoSuchElementException;
 
@@ -19,22 +21,35 @@ import java.util.NoSuchElementException;
 public class MemberController {
 
     private final KakaoService kakaoService;
+    private final MemberService memberService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     // 카카오 로그인 로직
     @ResponseBody
     @GetMapping("/oauth/kakao/login")
-    public ResponseEntity<LoginResponse> kakaoLogin(@RequestParam String code, HttpServletRequest request){
-        try{
+    public Result kakaoLogin(@RequestParam String code, HttpServletRequest request) {
+        try {
             logger.info("domain: " + request.getServerName());
-            return ResponseEntity.ok(kakaoService.kakaoLogin(code, request.getServerName()));
+            return new Result(ResultCode.SUCCESS, kakaoService.kakaoLogin(code, request.getServerName()));
         } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Item Not Found");
+            return new Result(ResultCode.FAIL, e.getMessage());
         }
     }
 
-//    @PutMapping("")
-//    public ResponseEntity<> updateMember() {
-//
-//    }
+    @PutMapping("")
+    public Result updateMember(
+            @RequestBody @Valid MemberForm memberForm,
+            @RequestHeader(value = "Authorization", required = false) String accessToken) {
+        try {
+            memberService.updateLearningLanguageAndLearningLevelAndAge(memberForm, accessToken);
+            return new Result(ResultCode.SUCCESS, null);
+        } catch (EntityNotFoundException | NoSuchElementException e) {
+            return new Result(ResultCode.FAIL, e.getMessage(), "400");
+        }
+    }
+
+    @GetMapping("/token")
+    public Result getToken() {
+        return new Result(ResultCode.SUCCESS, memberService.getAccessToken());
+    }
 }
