7c0d0d2a1ff2382c8d48f8322a47de5703156036
hayeongKo <polarpheno@icloud.com>
2024-08-22 01:42:50 +0900

    [FEAT] LanguageTale columnDefinition 추가

--- src/main/java/com/example/glowtales/domain/LanguageTale.java
+++ src/main/java/com/example/glowtales/domain/LanguageTale.java
@@ -3,10 +3,10 @@ package com.example.glowtales.domain;
 import com.example.glowtales.converter.YesOrNoConverter;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.sql.Clob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,8 +27,8 @@ public class LanguageTale {
     @JoinColumn(name = "tale_id")
     private Tale tale;
 
-    @Lob
-    private Clob story;
+    @Column(columnDefinition = "TEXT")
+    private String story;
 
     private String title;
 
@@ -39,4 +39,13 @@ public class LanguageTale {
     @OneToMany(mappedBy = "languageTale", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Quiz> quizList = new ArrayList<>();
 
+    @Builder
+    public LanguageTale(Language language, Tale tale, String story, String title, YesOrNo isLearned, Integer count) {
+        this.language = language;
+        this.tale = tale;
+        this.story = story;
+        this.title = title;
+        this.isLearned = isLearned;
+        this.count = count;
+    }
 }
