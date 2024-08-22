8e83fcaed5c7644b96035bf0f47b26aea8ce1147
hayeongKo <polarpheno@icloud.com>
2024-08-23 08:36:30 +0900

    [FEAT] findbymark, findbymarkandoriginword 추가

--- src/main/java/com/example/glowtales/repository/WordRepository.java
+++ src/main/java/com/example/glowtales/repository/WordRepository.java
@@ -4,4 +4,6 @@ import com.example.glowtales.domain.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
+    Word findByMarkAndOriginWord(String mark, Word originWord);
+    Word findByMark(String mark);
 }
