294f5f52b02de0d8d40645e83e2b4cec09258306
hayeongKo <polarpheno@icloud.com>
2024-08-23 08:36:47 +0900

    [FEAT] responsedto 생성

---
+++ src/main/java/com/example/glowtales/dto/response/quiz/KeyResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.glowtales.dto.response.quiz;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class KeyResponseDto {
+    private String mark;
+    private String translation;
+}
