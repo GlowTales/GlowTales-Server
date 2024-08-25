201be9dffb4f7f42bddbbf75954e33a9fa5dc858
hayeongKo <polarpheno@icloud.com>
2024-08-26 03:21:55 +0900

    [FEAT] 학습하기 여부 체크 로직 구현

--- src/main/java/com/example/glowtales/service/LanguageTaleService.java
+++ src/main/java/com/example/glowtales/service/LanguageTaleService.java
@@ -1,19 +1,28 @@
 package com.example.glowtales.service;
 
 import com.example.glowtales.domain.LanguageTale;
+import com.example.glowtales.domain.Quiz;
 import com.example.glowtales.domain.YesOrNo;
+import com.example.glowtales.dto.response.tale.IsLearnedDto;
+import com.example.glowtales.dto.response.tale.LanguageTaleAndIsLearnedAndLanguage;
 import com.example.glowtales.dto.response.tale.LanguageTaleDto;
 import com.example.glowtales.repository.LanguageTaleRepository;
+import com.example.glowtales.repository.QuizRepository;
+import com.example.glowtales.repository.TaleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class LanguageTaleService {
     private final LanguageTaleRepository languageTaleRepository;
+    private final TaleRepository taleRepository;
+    private final QuizRepository quizRepository;
 
     @Transactional
     public void updateIsLearned(LanguageTaleDto languageTaleDto) {
@@ -21,6 +30,31 @@ public class LanguageTaleService {
         languageTale.updateIsLearnedAndCountAndFirstQuizCount(YesOrNo.YES, languageTale.getCount()+1, languageTaleDto.getAnswerCounts());
     }
 
-//    public void getAllQuizInfo() {
-//    }
+    public IsLearnedDto getAllQuizInfo(Long taleId) {
+        List<LanguageTale> languageTales = languageTaleRepository.findByTale(taleRepository.findById(taleId).orElseThrow(() -> new NoSuchElementException("해당 동화가 존재하지 않습니다.")));
+
+        List<LanguageTaleAndIsLearnedAndLanguage> isLearnedInfos = new ArrayList<>();
+
+        for (LanguageTale languageTale : languageTales) {
+            List<Quiz> quiz = quizRepository.findByLanguageTale(languageTale);
+
+            LanguageTaleAndIsLearnedAndLanguage isLearnedInfo = LanguageTaleAndIsLearnedAndLanguage.builder()
+                    .languageTaleId(languageTale.getId())
+                    .languageId(languageTale.getLanguage().getId())
+                    .build();
+
+            if (quiz.isEmpty()) {
+                isLearnedInfo.setIsLearned(YesOrNo.NO);
+            } else {
+                isLearnedInfo.setIsLearned(YesOrNo.YES);
+            }
+
+            isLearnedInfos.add(isLearnedInfo);
+        }
+
+        IsLearnedDto isLearnedDto = new IsLearnedDto();
+        isLearnedDto.setIsLearnedInfos(isLearnedInfos);
+
+        return isLearnedDto;
+    }
 }
