fa7f6e46b0aef82d65ab7f9e6d5b7f53ae4541bc
hayeongKo <polarpheno@icloud.com>
2024-08-26 02:23:01 +0900

    [FIX] 동화 생성 시 createdDate 추가

--- src/main/java/com/example/glowtales/domain/Tale.java
+++ src/main/java/com/example/glowtales/domain/Tale.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Tale {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
