2f8c47b9c7a10860f0e34f2f81c562fb975de13e
hayeongKo <polarpheno@icloud.com>
2024-08-26 03:21:41 +0900

    [FEAT] findByTale 추가

--- src/main/java/com/example/glowtales/repository/LanguageTaleRepository.java
+++ src/main/java/com/example/glowtales/repository/LanguageTaleRepository.java
@@ -5,6 +5,9 @@ import com.example.glowtales.domain.LanguageTale;
 import com.example.glowtales.domain.Tale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LanguageTaleRepository extends JpaRepository<LanguageTale, Long> {
     LanguageTale findByLanguageAndTale(Language language, Tale tale);
+    List<LanguageTale> findByTale(Tale tale);
 }
