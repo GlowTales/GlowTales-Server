0d34b50da8186cb905393f76a1408f03c718e1d8
hayeongKo <polarpheno@icloud.com>
2024-08-26 01:24:39 +0900

    [FIX] service 수정 : 한 언어에 대한 반환값만 돌려주도록 수정

--- src/main/java/com/example/glowtales/service/MemberService.java
+++ src/main/java/com/example/glowtales/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.glowtales.service;
 import com.example.glowtales.config.jwt.JwtTokenProvider;
 import com.example.glowtales.domain.LearningLanguage;
 import com.example.glowtales.domain.Member;
+import com.example.glowtales.domain.YesOrNo;
 import com.example.glowtales.dto.request.MemberForm;
 import com.example.glowtales.dto.response.member.LearningLanguageResponseDto;
 import com.example.glowtales.repository.LanguageRepository;
@@ -55,14 +56,34 @@ public class MemberService {
         return jwtTokenProvider.accessTokenGenerate("1", new Date((new Date()).getTime() + 1000 * 60 * 60));
     }
 
-    public List<LearningLanguageResponseDto> getLanguageLearningDataByMemberId(String accessToken) {
-        if (accessToken==null){
-            throw new RuntimeException("accessToken이 null입니다");}
-        Member member=findMemberByAccessToken(accessToken);
-        if (member==null){
-            throw new RuntimeException("해당 아이디에 맞는 멤버가 없습니다.");}
-        List<LearningLanguage> learningLanguages = learningLanguageRepository.findByMemberId(member.getId());
+//    public List<LearningLanguageResponseDto> getLanguageLearningDataByMemberId(String accessToken) {
+//        if (accessToken==null){
+//            throw new RuntimeException("accessToken이 null입니다");}
+//        Member member=findMemberByAccessToken(accessToken);
+//        if (member==null){
+//            throw new RuntimeException("해당 아이디에 맞는 멤버가 없습니다.");}
+//        List<LearningLanguage> learningLanguages = learningLanguageRepository.findByMemberId(member.getId());
+//
+//        return LearningLanguageResponseDto.from(learningLanguages);
+//    }
 
-        return LearningLanguageResponseDto.from(learningLanguages);
+    public LearningLanguageResponseDto getLearningLevel(String accessToken, Long languageId) {
+        Member member = findMemberByAccessToken(accessToken);
+        if (member == null) {
+            throw new EntityNotFoundException("해당 회원이 존재하지 않습니다.");
+        }
+        LearningLanguage learningLanguage = learningLanguageRepository.findByMemberAndLanguage(member, languageRepository.findById(languageId).orElseThrow(() -> new NoSuchElementException("해당 언어가 존재하지 않습니다.")));
+
+        if (learningLanguage == null) {
+            return LearningLanguageResponseDto.builder()
+                    .isLearned(YesOrNo.NO)
+                    .build();
+        } else {
+            return LearningLanguageResponseDto.builder()
+                    .isLearned(YesOrNo.YES)
+                    .learningLevel(learningLanguage.getLearningLevel())
+                    .build();
+        }
+
     }
 }
