c4bed280627a785f5795e269bad1782f213a61b1
hayeongKo <polarpheno@icloud.com>
2024-08-26 23:50:09 +0900

    [REFACTOR] 퀴즈 생성 시 학습 언어에 대한 데이터가 없을 경우 저장 로직 구현

--- src/main/java/com/example/glowtales/dto/request/QuizForm.java
+++ src/main/java/com/example/glowtales/dto/request/QuizForm.java
@@ -1,5 +1,6 @@
 package com.example.glowtales.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,5 +10,6 @@ import lombok.Setter;
 public class QuizForm {
     @NotNull
     private Long languageTaleId;
+    @NotBlank
     private String learningLevel;
 }
