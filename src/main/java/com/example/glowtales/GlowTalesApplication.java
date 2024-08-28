6a5a7b2ba5b882959ec3459b710c544bb1a0eb6d
eunsu02 <regulus_leo08@naver.com>
2024-08-28 17:16:35 +0900

    [FIX] 서버 시간대 한국으로 변경

--- src/main/java/com/example/glowtales/GlowTalesApplication.java
+++ src/main/java/com/example/glowtales/GlowTalesApplication.java
@@ -1,8 +1,11 @@
 package com.example.glowtales;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,5 +14,8 @@ public class GlowTalesApplication {
     public static void main(String[] args) {
         SpringApplication.run(GlowTalesApplication.class, args);
     }
-
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
